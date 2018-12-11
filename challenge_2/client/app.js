e1af92ff6ba23abf248908a98545b6dd21b4912c
klarok <karaylu@gmail.com>
2018-12-11 11:46:57 -0800

    Server responds to requests, serves static files.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,21 @@
+let SUBMIT  = 'submit-JSON';
+let INPUT = 'input-JSON';
 
+let submitButton = document.getElementById(SUBMIT);
+let textInput = document.getElementById(INPUT);
 
+submitButton.addEventListener('click', () => {
+	let input = JSON.stringify(textInput.value);
+	// let input = textInput.value;
+	console.log(input);
+	fetch('/post', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json; charset=utf-8'
+		},
+		body: input
+	})
+		.then(res => {
+			console.log('Heard back from server', res);
+		});
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
+const port = 3000;
 
+app.use(express.static('client'));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+	console.log('Now listening on port ' + port + '...');
+})
+
+///////////// ROUTES /////////////
+
+app.get('/', (req, res) => {
+	res.send('hey there');
+});
+
+app.post('/post', (req, res) => {
+	console.log(req.body);
+	res.sendStatus('201');
+});
